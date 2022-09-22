c0a604381e4d8ce662ccd9fccc8c48f7eb8c49ea
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-09-22 15:46:20 -0300

    [This is the First]

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import * as THREE from 'three'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  scene = new THREE.Scene();
+  
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Object.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as THREE from 'three'
+
+export const Object = () => {
+    const geometry = new THREE.BoxBufferGeometry(1,1,1)
+
+  return (
+    <div>Object</div>
+  )
+}
