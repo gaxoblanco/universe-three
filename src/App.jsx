0387b229b5b7a54d5ceec8fe2b75869f8b2445e7
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-09-23 18:33:13 -0300

    [Objetivo Completo]
    Replique una web que vi, y le inclui las opciones para que tenga interactividad.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,69 @@
-import { useState } from 'react'
+import { useContext, useRef, useState } from 'react'
+import { createRoot } from 'react-dom/client'
 import * as THREE from 'three'
-import reactLogo from './assets/react.svg'
+import {Canvas} from '@react-three/fiber'
+import {PerspectiveCamera, Text3D, OrbitControls} from '@react-three/drei'
 import './App.css'
 
+import {Object} from './components/object'
+
 function App() {
-  scene = new THREE.Scene();
-  
+  const [state, setState] = useState(0);
+  const [text, setText] = useState('');
+  let array = [];
+
+  const sphere = ()=>{
+    setState(0)
+  }
+  const torus = ()=>{
+    setState(1)
+  }
+  const tetrahedron = ()=>{
+    setState(2)
+  }
+
+
+  const onSearchValueChange = (event)=>{
+    console.log('desde el Search',event.target.value);
+    setText(event.target.value)
+}
 
   return (
-    <div className="App">
+    <div className="App" >
+      <Canvas style={{ width: '80vw', height: '80vh'}}>
+        <PerspectiveCamera 
+            makeDefault //para que esta camara sea por default
+            position ={[0,-10,0]} 
+            fov={60}
+        />
+          <ambientLight />
+          <pointLight position={[10,10,10]}/>
+          <mesh>
 
+          <Object state={state}/>
+          </mesh>
+          
+          
+          <Text3D font={'./public/fonts/helvetiker_regular.typeface.json'}>
+            {text}
+            <meshNormalMaterial />
+          </Text3D>
+          <OrbitControls  />  
+      </Canvas>
+      <div className="inputs-container">
+        <input 
+          type="text" 
+          name="name" 
+          placeholder='Escribe algo'
+          onChange={onSearchValueChange}
+        />
+        <button type='button' onClick={()=> sphere()}>Esferas</button>
+        <button type='button' onClick={()=> torus()}>Torus</button>
+        <button type='button' onClick={()=> tetrahedron()}>Tetrahedron</button>
+      </div>
     </div>
   )
 }
+
 
 export default App
--- src/components/Object.jsx
+++ src/components/Object.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Sphere, Tetrahedron, Torus, TorusKnot} from '@react-three/drei'
 import * as THREE from 'three'
 
-export const Object = () => {
-    const geometry = new THREE.BoxBufferGeometry(1,1,1)
+export const Object = ({state}) => {
+
+    // const randomX = (Math.random() - 0.5) * 10
+    // const randomY = (Math.random() - 0.5) * 10
+    // const randomZ = (Math.random() - 0.5) * 10
+    // const scale = Math.random()
+
+const obj = []
+
+const esfera =[]
+for (let i = 0; i < 100; i++){
+  const randomX = (Math.random() - 0.5) * 30
+  const randomY = (Math.random() - 0.5) * 30
+  const randomZ = (Math.random() - 0.5) * 30
+  const scale = Math.random()
+  esfera.push(<Sphere position={[randomX, randomY, randomZ]} scale={scale}><meshBasicMaterial color='hotpink' /></Sphere>)
+}
+
+const toru =[]
+for (let i = 0; i < 100; i++){
+  const randomX = (Math.random() - 0.5) * 30
+  const randomY = (Math.random() - 0.5) * 30
+  const randomZ = (Math.random() - 0.5) * 30
+  const scale = Math.random()
+  toru.push(<Torus position={[randomX, randomY, randomZ]} scale={scale}><meshBasicMaterial color='hotpink' /></Torus>)
+}
+
+const tetrahedron =[]
+for (let i = 0; i < 100; i++){
+  const randomX = (Math.random() - 0.5) * 30
+  const randomY = (Math.random() - 0.5) * 30
+  const randomZ = (Math.random() - 0.5) * 30
+  const scale = Math.random()
+  tetrahedron.push(<Tetrahedron position={[randomX, randomY, randomZ]} scale={scale}><meshBasicMaterial color='hotpink' /></Tetrahedron>)
+}
+
 
   return (
-    <div>Object</div>
+    <mesh>
+    {state == 0 ? 
+      esfera
+      : (state == 1 ? toru
+        : tetrahedron)
+    }
+   </mesh>
   )
 }
+ 
