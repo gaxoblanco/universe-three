ef3fbfb8af455e9a6f86e89f24d6870ae81901af
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-09-24 21:44:05 -0300

    [Panel]
    Panel terminado. El objetivo del proyecto se cumplio.

--- src/components/ComandPanel.jsx
+++ src/components/ComandPanel.jsx
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import {AppContext} from '../context/AppContex';
 
-export const ComandPanel = ({setState, setMat, setTextMat, setText}) => {
+export const ComandPanel = ({setState, setMat, setTextMat, setText, setPanel}) => {
 
 
 const handleObj = (n)=>{
@@ -22,7 +22,7 @@ const handleObj = (n)=>{
   return (
     <span className="span-comandPanel-container">
       <div className="options-container">
-        <button className="down-button">
+        <button type='button' className="down-button" onClick={()=>{setPanel(false)}}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"  viewBox="0 0 16 16">
             <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
           </svg>
